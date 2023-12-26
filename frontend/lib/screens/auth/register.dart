52e4cd85c6b733b9c14167e610e215bcb247e019
maikkusman <42khan0@gmail.com>
2023-12-26 15:01:09 +0500

    Add & update Frontend

---
+++ frontend/lib/screens/auth/register.dart
@@ -0,0 +1,195 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../widgets/custombutton.dart';
+import '../homepage.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final focus = FocusNode();
+  final passwordController = TextEditingController();
+  final confirmpasswordController = TextEditingController();
+  bool isObsecured = true;
+  _storeOnboardInfo() async {
+    int isViewed = 0;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('onBoard', isViewed);
+  }
+
+  _savePassword(String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('password', password);
+  }
+
+  final GlobalKey<FormState> _registerformKey = GlobalKey<FormState>();
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'passwords must have at least one special character')
+  ]);
+
+  void validate(BuildContext context) async {
+    final FormState form = _registerformKey.currentState!;
+    if (form.validate()) {
+      await _storeOnboardInfo();
+      await _savePassword(confirmpasswordController.text.trim());
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    } else {
+      const snackbar = SnackBar(
+        content: Text("Form is invalid"),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    }
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    confirmpasswordController.dispose();
+    focus.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.07,
+            ),
+            child: Form(
+              key: _registerformKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: size.height * 0.07,
+                  ),
+                  SvgPicture.asset(
+                    'assets/secure_files.svg',
+                    height: size.height * 0.2,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  Text(
+                    'Register a master password',
+                    style: TextStyle(
+                      // fontWeight: FontWeight.w500,
+                      fontSize: 18,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.07,
+                  ),
+                  TextFormField(
+                    onFieldSubmitted: (value) {
+                      FocusScope.of(context).requestFocus(focus);
+                    },
+                    obscureText: isObsecured,
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.next,
+                    validator: passwordValidator.call,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      border: const OutlineInputBorder(),
+                      suffix: InkWell(
+                        child: Icon(
+                          isObsecured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            isObsecured = !isObsecured;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  TextFormField(
+                    focusNode: focus,
+                    obscureText: isObsecured,
+                    controller: confirmpasswordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.done,
+                    validator: (val) => MatchValidator(
+                            errorText: 'passwords do not match')
+                        .validateMatch(val!, passwordController.text.trim()),
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      filled: true,
+                      border: const OutlineInputBorder(),
+                      suffix: InkWell(
+                        child: Icon(
+                          isObsecured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            isObsecured = !isObsecured;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.04,
+                  ),
+                  CustomButton(
+                    ontap: () {
+                      validate(context);
+                    },
+                    buttontext: 'Register',
+                  ),
+                  SizedBox(
+                    height: size.height * 0.07,
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  const Text(
+                    'Note that if the master password is lost,the stored '
+                    'data cannot be recovered because of the missing '
+                    'sync option. it is strongly recommended that you '
+                    'backup your  data at regular intervals.',
+                    style: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/screens/onboardingpage.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frontend/provider/themeprovider.dart';
+import 'package:provider/provider.dart';
+import '../const/consts.dart';
+import '../widgets/custombutton.dart';
+import 'auth/register.dart';
+
+class OnBoardingSceen extends StatefulWidget {
+  const OnBoardingSceen({super.key});
+
+  @override
+  State<OnBoardingSceen> createState() => _OnBoardingSceenState();
+}
+
+class _OnBoardingSceenState extends State<OnBoardingSceen> {
+  int currentIndex = 0;
+  PageController _controller = PageController();
+  @override
+  void initState() {
+    _controller = PageController(
+      initialPage: 0,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: context.watch<ThemeProvider>().getDarkTheme
+            ? Colors.black
+            : Colors.white,
+        statusBarIconBrightness: context.watch<ThemeProvider>().getDarkTheme
+            ? Brightness.light
+            : Brightness.dark,
+      ),
+      child: Scaffold(
+        // appBar: AppBar(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  controller: _controller,
+                  onPageChanged: (int index) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  itemCount: contents.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: size.width * 0.07,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: size.height * 0.07,
+                          ),
+                          SvgPicture.asset(
+                            contents[index].image,
+                            height: size.height * 0.2,
+                          ),
+                          SizedBox(
+                            height: size.height * 0.07,
+                          ),
+                          Text(
+                            contents[index].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 24,
+                            ),
+                          ),
+                          SizedBox(
+                            height: size.height * 0.07,
+                          ),
+                          Text(
+                            contents[index].description,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  contents.length,
+                  (index) => buildDots(index, context),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(30),
+                child: CustomButton(
+                  ontap: () {
+                    if (currentIndex == contents.length - 1) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterPage(),
+                        ),
+                      );
+                    }
+                    _controller.nextPage(
+                      duration: const Duration(
+                        microseconds: 100,
+                      ),
+                      curve: Curves.bounceIn,
+                    );
+                  },
+                  buttontext:
+                      currentIndex == contents.length - 1 ? "Continue" : 'Next',
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDots(int index, BuildContext context) {
+    return Container(
+      height: 10,
+      width: currentIndex == index ? 25 : 10,
+      margin: const EdgeInsets.only(
+        right: 5,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
+
+class OnBoardingContent {
+  String image;
+  String title;
+  String description;
+  OnBoardingContent({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
+
+List<OnBoardingContent> contents = [
+  OnBoardingContent(
+    image: 'assets/vault.svg',
+    title: Consts.TITLE_1,
+    description: Consts.SUBTITLE_1,
+  ),
+  OnBoardingContent(
+    image: 'assets/secure.svg',
+    title: Consts.TITLE_2,
+    description: Consts.SUBTITLE_2,
+  ),
+];
